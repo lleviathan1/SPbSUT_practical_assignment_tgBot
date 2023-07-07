543d23eac2172e792be051bb87dee037a5b93761
sumenkov <sumenkov@protei-lab.ru>
2023-07-08 01:13:56 +0300

    Изменил main.py
    Добавил базового бота для теста

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import telebot
+import json
+
+bot = telebot.TeleBot("API-Token")
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+
 def main():
     print("Hello Telegram")
+    bot.infinity_polling()
+
 
 if __name__ == "__main__":
     main()
