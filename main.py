3ca69d22a4d92cac22b077b4cecd13e00dd81b37
sumenkov <sumenkov@protei-lab.ru>
2023-07-09 01:51:17 +0300

    Изменил main.py
    Добавил логирование и изменение состояния

--- main.py
+++ main.py
@@ -14,36 +14,57 @@ bot = telebot.TeleBot(config.parse("config/config.json")["token"])
 
 usersState = {}
 
+
 @bot.message_handler(commands=['start'])
 def request_start(message):
-    logging.info( f"Новое сообщение от пользователя id: {message.from_user.id} -> {message.text}")
+    logging.info(f"Новое сообщение от пользователя id: {message.from_user.id} -> {message.text}")
     usersState[str(message.from_user.id)] = "functionSelection"
     logging.info(f'Пользователь id {message.from_user.id} сменил состояние на {usersState[str(message.from_user.id)]}')
     bot.reply_to(message, "Привет, выбери действие", reply_markup=keyboard.getStartKeyboard())
 
+
 @bot.message_handler(commands=['help'])
 def request_help(message):
-    logging.info( f"Новое сообщение от пользователя id: {message.from_user.id} -> {message.text}")
+    logging.info(f"Новое сообщение от пользователя id: {message.from_user.id} -> {message.text}")
+    lastState = usersState.get(str(message.from_user.id))
     usersState[str(message.from_user.id)] = "functionSelection"
-    logging.info(f'Пользователь id {message.from_user.id} сменил состояние на {usersState[str(message.from_user.id)]}')
+    logging.info(
+        f'Пользователь id {message.from_user.id} сменил состояние c {lastState} на {usersState[str(message.from_user.id)]}')
     bot.reply_to(message, "Что-то помогающее пользователю", reply_markup=keyboard.getStartKeyboard())
 
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    if usersState[str(message.from_user.id)] == "functionSelection":
+    logging.info(f"Новое сообщение от пользователя id: {message.from_user.id} -> {message.text}")
+    if usersState.get(str(message.from_user.id)) in ["functionSelection", None]:
+        lastState = usersState.get(str(message.from_user.id))
         if message.text == "Переводчик":
-            a = 1
+            usersState[str(message.from_user.id)] = "translate"
+            logging.info(
+                f'Пользователь id {message.from_user.id} сменил состояние на {usersState[str(message.from_user.id)]}')
         elif message.text == "Калькулятор":
-            a=2
+            usersState[str(message.from_user.id)] = "calc"
+            logging.info(
+                f'Пользователь id {message.from_user.id} сменил состояние на {usersState[str(message.from_user.id)]}')
         elif message.text == "Прогноз погоды":
-            a=3
+            usersState[str(message.from_user.id)] = "weather"
+            logging.info(
+                f'Пользователь id {message.from_user.id} сменил состояние на {usersState[str(message.from_user.id)]}')
         elif message.text == "Мем дня":
-            a=4
+            usersState[str(message.from_user.id)] = "meme"
+            logging.info(
+                f'Пользователь id {message.from_user.id} сменил состояние на {usersState[str(message.from_user.id)]}')
+        else:
+            logging.warning(f"Пользователь отправил некоректную команду {message.text}. Сообщение проигнорировано")
+    else:
+        logging.warning(
+            f"Состояние пользователя {usersState.get(str(message.from_user.id))}, ожидалось functionSelection. Сообщение проигнорировано")
+
 
 def main():
     logging.info("Бот стартовал")
     bot.infinity_polling()
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, filename="loging/info.log", filemode="w",
