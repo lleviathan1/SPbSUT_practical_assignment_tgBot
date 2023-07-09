58859fa0935f2ca5ef34d71c27458d01a9ec6d3e
sumenkov <sumenkov@protei-lab.ru>
2023-07-09 03:35:40 +0300

    Изменил keyboard.py
    Добавил примеры для калькулятора

--- source/keyboard.py
+++ source/keyboard.py
@@ -1,10 +1,25 @@
 from telebot import types
 
+
 def getStartKeyboard():
     markup = types.ReplyKeyboardMarkup()
-    itembtn1 = types.KeyboardButton('Переводчик')
-    itembtn2 = types.KeyboardButton('Калькулятор')
-    itembtn3 = types.KeyboardButton('Прогноз погоды')
-    itembtn4 = types.KeyboardButton('Мем дня')
-    markup.add(itembtn1, itembtn2, itembtn3, itembtn4)
+    btn1 = types.InlineKeyboardButton('Переводчик', callback_data='Переводчик')
+    btn2 = types.InlineKeyboardButton('Калькулятор', callback_data='Калькулятор')
+    btn3 = types.InlineKeyboardButton('Прогноз погоды', callback_data='Прогноз погоды')
+    btn4 = types.InlineKeyboardButton('Мем дня', callback_data='Мем дня')
+    markup.add(btn1)
+    markup.add(btn2)
+    markup.add(btn3)
+    markup.add(btn4)
+    return markup
+
+
+def getCalcExamples():
+    markup = types.ReplyKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton('0.1 + 0.3 * e ** pi', callback_data='0.1 + 0.3 * e ** pi')
+    btn2 = types.InlineKeyboardButton('6/(8-2**3)', callback_data='6/(8-2**3)')
+    btn3 = types.InlineKeyboardButton('(((-6)(1)))**-pi', callback_data='(((-6)(1)))**-pi')
+    markup.add(btn1)
+    markup.add(btn2)
+    markup.add(btn3)
     return markup
