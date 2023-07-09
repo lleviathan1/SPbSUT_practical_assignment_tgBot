1afbb32246ca7fe2f1e1d68470dbd83b5e93d5ab
sumenkov <sumenkov@protei-lab.ru>
2023-07-09 03:48:19 +0300

    Добавил сложный Unit тест

--- tests.py
+++ tests.py
@@ -6,7 +6,8 @@ class calcTest(unittest.TestCase):
     def test_calc_from_string(self):
         self.assertEqual(str(calc.calc_from_string("1")), "1")
         self.assertEqual(str(calc.calc_from_string("2**10")), "1024")
-        self.assertNotEquals(str(calc.calc_from_string("0/0")), "0")
+        self.assertNotEqual(str(calc.calc_from_string("0/0")), "0")
+        self.assertEqual(str(calc.calc_from_string("0.1 + 0.3 * e ** pi")), "7.042207789833778")
 
 
 
