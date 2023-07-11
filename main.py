056e459e8b80d0cc4b99d6e041eb380fe9d861f3
sumenkov <sumenkov@protei-lab.ru>
2023-07-12 02:56:38 +0300

    Добавила переводчик

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ import logging
 import json
 
 # Конфигурирование логов
-logging.basicConfig(level=logging.INFO, filename="loging/info.log", filemode="w",
+logging.basicConfig(level=logging.INFO, filename="logging/info.log", filemode="w",
                         format="%(asctime)s %(levelname)s %(message)s")
 
 # Вспомогательные функции
 import source.config as config
 import source.keyboard as keyboardd
+import source.sl_log as log
 
 # SL - Сервис логика
 import source.meme as meme
@@ -35,10 +36,7 @@ usersState = {}
 @bot.message_handler(commands=['start'])
 def request_start(message):
     logging.info(f"Новое сообщение от пользователя id: {message.from_user.id} -> {message.text}")
-    lastState = usersState.get(str(message.from_user.id))
-    usersState[str(message.from_user.id)] = "functionSelection"
-    logging.info(
-        f'Пользователь id {message.from_user.id} сменил состояние c {lastState} на {usersState[str(message.from_user.id)]}')
+    log.sl_log_state("functionSelection", message.from_user.id, usersState)
     bot.reply_to(message, "Привет, выбери действие", reply_markup=keyboardd.getStartKeyboard())
 
 
@@ -57,29 +55,25 @@ def request_help(message):
 def echo_all(message):
     logging.info(f"Новое сообщение от пользователя id: {message.from_user.id} -> {message.text}")
     if usersState.get(str(message.from_user.id)) in ["functionSelection", None]:
-        lastState = usersState.get(str(message.from_user.id))
         if message.text == "Переводчик":
-            usersState[str(message.from_user.id)] = "translate"
-            logging.info(
-                f'Пользователь id {message.from_user.id} сменил состояние c {lastState} на {usersState[str(message.from_user.id)]}')
+            log.sl_log_state("translate", message.from_user.id, usersState)
+            translate.request_phrase(message, bot, usersState)
         elif message.text == "Калькулятор":
-            usersState[str(message.from_user.id)] = "calc"
-            logging.info(
-                f'Пользователь id {message.from_user.id} сменил состояние c {lastState} на {usersState[str(message.from_user.id)]}')
+            log.sl_log_state("calc", message.from_user.id, usersState)
             calc.calc_send_request(message, bot, usersState)
         elif message.text == "Прогноз погоды":
-            usersState[str(message.from_user.id)] = "weather"
-            logging.info(
-                f'Пользователь id {message.from_user.id} сменил состояние c {lastState} на {usersState[str(message.from_user.id)]}')
+            log.sl_log_state("weather", message.from_user.id, usersState)
+
         elif message.text == "Мем дня":
-            usersState[str(message.from_user.id)] = "meme"
-            logging.info(
-                f'Пользователь id {message.from_user.id} сменил состояние c {lastState} на {usersState[str(message.from_user.id)]}')
+            log.sl_log_state("mems", message.from_user.id, usersState)
             meme.send_meme(message, bot, usersState)
         else:
             logging.warning(f"Пользователь отправил некоректную команду {message.text}. Сообщение проигнорировано")
     elif usersState.get(str(message.from_user.id)) == "waitCalcResponse":
         calc.calc_send_responce(message, bot, usersState)
+    elif usersState.get(str(message.from_user.id)) == "waitPhraseToTranslate":
+        log.sl_log_state("asyncRequestTranslate", message.from_user.id, usersState)
+        translate.response_translate_text(message, bot, usersState)
     else:
         logging.warning(
             f"Состояние пользователя {usersState.get(str(message.from_user.id))}. Сообщение проигнорировано")
